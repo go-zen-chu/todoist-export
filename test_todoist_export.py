71a67246dbb5f3233ea9853a37e6d9c5119f7c06
Akira Masuda <masuda.akira1014@gmail.com>
2020-12-30 05:08:19 +0900

    impl test with mock

--- test_todoist_export.py
+++ test_todoist_export.py
@@ -1,4 +1,6 @@
 from todoist_export import TodoistAPIClient, TodoistExport
+from unittest import mock
+from datetime import datetime, timedelta
 
 
 def test_TodoistAPIClient___init__():
@@ -10,3 +12,11 @@ def test_TodoistExport___init__():
     cli = TodoistAPIClient("todoist_token")
     exp = TodoistExport(cli)
     assert exp is not None
+
+
+def test_TodoistExport_export():
+    mcli = mock.MagicMock(return_value="some return value")
+    exp = TodoistExport(mcli)
+    now = datetime.now()
+    yesterday = now - timedelta(days=1)
+    assert exp.export(from_dt=yesterday, to_dt=now, format="yaml") == "TBD"
--- todoist_export.py
+++ todoist_export.py
@@ -13,7 +13,7 @@ class TodoistExport:
     def __init__(self, cli: TodoistAPIClient):
         self.cli = cli
 
-    def export(self, from_dt: datetime, to_dt: datetime, format: str) -> str:
+    def export(self, from_dt: datetime, to_dt: datetime, format: str = "yaml") -> str:
         return "TBD"
         # activities = api.activity.get(object_type="item", event_type="completed", page=1)
         # pprint.pprint(activities)
