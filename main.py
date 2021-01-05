78418ce2a09f4da971d73075b474f30a29702dcb
Akira Masuda <masuda.akira1014@gmail.com>
2021-01-05 09:41:33 +0900

    update

--- main.py
+++ main.py
@@ -2,11 +2,33 @@
 import argparse
 import os
 from todoist_export import TodoistAPIClient, TodoistExport
+from datetime import date, datetime, timedelta
+
+
+def date_validation(date_str):
+    try:
+        return date.fromisoformat(date_str)
+    except ValueError as e:
+        raise argparse.ArgumentTypeError("{}: Date must be in ISO format. ex. 2020-01-01".format(e))
+
 
 if __name__ == '__main__':
+    now = datetime.now()
+    yesterday = now - timedelta(days=1)
+
     parser = argparse.ArgumentParser(description='export todoist data with certain format')
-    parser.add_argument('--api-token', default=os.environ.get('TODOIST_API_TOKEN'))
+    parser.add_argument('--api-token',
+                        default=os.environ.get('TODOIST_API_TOKEN'))
+    parser.add_argument('--from-date',
+                        type=date_validation,
+                        default=yesterday.strftime("%Y-%m-%d"),
+                        help="From date where you retrieve data. Must be ISO format (YYYY-MM-DD)")
+    parser.add_argument('--to-date',
+                        type=date_validation,
+                        default=now.strftime("%Y-%m-%d"),
+                        help="To date where you retrieve data. Must be ISO format (YYYY-MM-DD)")
     args = parser.parse_args()
     cli = TodoistAPIClient(args.api_token)
     exp = TodoistExport(cli)
+    exp.export(from_dt=args.from_date, to_dt=args.to_date)
     print("done")
--- todoist_export.py
+++ todoist_export.py
@@ -1,5 +1,7 @@
 import todoist
-from datetime import datetime
+from datetime import datetime, date
+import pprint
+from typing import List
 
 
 class TodoistAPIClient:
@@ -8,12 +10,22 @@ class TodoistAPIClient:
         # auth Todoist API
         self.api = todoist.TodoistAPI(token)
 
+    def get_completed_activities(self, from_dt: datetime, to_dt: datetime) -> List:
+        activities = []
+        # params: https://developer.todoist.com/sync/v8/?shell#get-activity-logs
+        events = self.api.activity.get(object_type="item", event_type="completed", limit=100)
+        # check events are in datetime
+        for ev in events['events']:
+            ev_dt = datetime.strftime(ev.'%Y-%m-%dT%H:%M:%SZ')
+        return activities
+
 
 class TodoistExport:
     def __init__(self, cli: TodoistAPIClient):
         self.cli = cli
 
     def export(self, from_dt: datetime, to_dt: datetime, format: str = "yaml") -> str:
+        acts = self.cli.get_completed_activities()
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(acts)
         return "TBD"
-        # activities = api.activity.get(object_type="item", event_type="completed", page=1)
-        # pprint.pprint(activities)
