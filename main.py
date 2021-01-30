29aff8895cadd3aea20b4a164419d7a2273988a4
Akira Masuda <masuda.akira1014@gmail.com>
2021-01-30 16:40:49 +0900

    format

--- main.py
+++ main.py
@@ -21,49 +21,68 @@ def date_validation(date_str: str):
     try:
         # support local timezone
         tz = tzlocal.get_localzone()
-        naive_dt = datetime.strptime(date_str, '%Y-%m-%d')
+        naive_dt = datetime.strptime(date_str, "%Y-%m-%d")
         dt = naive_dt.astimezone(tz=tz)
         return dt
     except ValueError as e:
-        raise argparse.ArgumentTypeError('{}: Date must be in ISO format. e.g. 2020-01-01'.format(e))
+        raise argparse.ArgumentTypeError(
+            "{}: Date must be in ISO format. e.g. 2020-01-01".format(e)
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     now = datetime.now()
     yesterday = now - timedelta(days=1)
-    parser = argparse.ArgumentParser(description='export todoist data with certain format')
-    parser.add_argument('--api-token',
-                        type=str,
-                        default=os.environ.get('TODOIST_API_TOKEN'),
-                        help='todoist API token')
-    parser.add_argument('--data',
-                        type=str,
-                        choices=['daily-report'],
-                        help='Data type to be exported',
-                        required=True)
-    parser.add_argument('--format',
-                        type=str,
-                        default='yaml',
-                        choices=['yaml'],
-                        help='Data export format')
-    parser.add_argument('--pj-filter',
-                        type=str,
-                        default='.*',
-                        help='Project filter regular expression')
-    parser.add_argument('--from-date',
-                        type=date_validation,
-                        default=yesterday.strftime('%Y-%m-%d'),
-                        help='From date where you retrieve data. Must be ISO format (YYYY-MM-DD)')
-    parser.add_argument('--until-date',
-                        type=date_validation,
-                        default=now.strftime('%Y-%m-%d'),
-                        help='Until date where you retrieve data. Must be ISO format (YYYY-MM-DD)')
+    parser = argparse.ArgumentParser(
+        description="export todoist data with certain format"
+    )
+    parser.add_argument(
+        "--api-token",
+        type=str,
+        default=os.environ.get("TODOIST_API_TOKEN"),
+        help="todoist API token",
+    )
+    parser.add_argument(
+        "--data",
+        type=str,
+        choices=["daily-report"],
+        help="Data type to be exported",
+        required=True,
+    )
+    parser.add_argument(
+        "--format",
+        type=str,
+        default="yaml",
+        choices=["yaml"],
+        help="Data export format",
+    )
+    parser.add_argument(
+        "--pj-filter", type=str, default=".*", help="Project filter regular expression"
+    )
+    parser.add_argument(
+        "--from-date",
+        type=date_validation,
+        default=yesterday.strftime("%Y-%m-%d"),
+        help="From date where you retrieve data. Must be ISO format (YYYY-MM-DD)",
+    )
+    parser.add_argument(
+        "--until-date",
+        type=date_validation,
+        default=now.strftime("%Y-%m-%d"),
+        help="Until date where you retrieve data. Must be ISO format (YYYY-MM-DD)",
+    )
     args = parser.parse_args()
     cli = TodoistAPIClient(args.api_token)
     exp = TodoistExport(cli)
-    if args.data == 'daily-report':
+    if args.data == "daily-report":
         tz = tzlocal.get_localzone()
-        res = exp.export_daily_report(from_dt=args.from_date, until_dt=args.until_date, tz=tz, format=args.format)
+        res = exp.export_daily_report(
+            from_dt=args.from_date,
+            until_dt=args.until_date,
+            pj_filter=args.pj_filter,
+            tz=tz,
+            format=args.format,
+        )
         print(res)
     else:
-        raise argparse.ArgumentError('--data is invalid')
+        raise argparse.ArgumentError("--data is invalid")
--- test_todoist_export.py
+++ test_todoist_export.py
@@ -4,24 +4,24 @@ from datetime import datetime, timezone
 
 
 def test_TodoistAPIClient___init__():
-    cli = TodoistAPIClient('todoist_token')
+    cli = TodoistAPIClient("todoist_token")
     assert cli is not None
 
 
 def test_TodoistAPIClient_get_completed_activities():
-    cli = TodoistAPIClient('todoist_token')
+    cli = TodoistAPIClient("todoist_token")
     m = mock.MagicMock(return_value=activity_logs_valid_data)
     cli.api.activity.get = m
-    from_dt = datetime.strptime('2020-11-10T10:02:03Z', '%Y-%m-%dT%H:%M:%SZ')
+    from_dt = datetime.strptime("2020-11-10T10:02:03Z", "%Y-%m-%dT%H:%M:%SZ")
     from_dt = from_dt.replace(tzinfo=timezone.utc)
-    until_dt = datetime.strptime('2021-01-10T10:02:03Z', '%Y-%m-%dT%H:%M:%SZ')
+    until_dt = datetime.strptime("2021-01-10T10:02:03Z", "%Y-%m-%dT%H:%M:%SZ")
     until_dt = until_dt.replace(tzinfo=timezone.utc)
     acts = cli.get_completed_activities(from_dt=from_dt, until_dt=until_dt)
     assert len(acts) == 3
 
 
 def test_TodoistExport___init__():
-    cli = TodoistAPIClient('todoist_token')
+    cli = TodoistAPIClient("todoist_token")
     exp = TodoistExport(cli)
     assert exp is not None
 
@@ -33,114 +33,107 @@ def test_TodoistExport_export_daily_report():
     exp = TodoistExport(mcli)
 
     # test from, until
-    from_dt = datetime.strptime('2020-11-10T10:02:03Z', '%Y-%m-%dT%H:%M:%SZ')
+    from_dt = datetime.strptime("2020-11-10T10:02:03Z", "%Y-%m-%dT%H:%M:%SZ")
     from_dt = from_dt.replace(tzinfo=timezone.utc)
-    until_dt = datetime.strptime('2021-01-10T10:02:03Z', '%Y-%m-%dT%H:%M:%SZ')
+    until_dt = datetime.strptime("2021-01-10T10:02:03Z", "%Y-%m-%dT%H:%M:%SZ")
     until_dt = until_dt.replace(tzinfo=timezone.utc)
-    assert exp.export_daily_report(from_dt=from_dt, until_dt=until_dt) == daily_report_str
+    assert (
+        exp.export_daily_report(from_dt=from_dt, until_dt=until_dt) == daily_report_str
+    )
 
     # test pj_filter
-    filter1 = '.*'
-    assert exp.export_daily_report(from_dt=from_dt, until_dt=until_dt, pj_filter=filter1) == daily_report_str
-    filter2 = 'pj1+'
-    assert exp.export_daily_report(from_dt=from_dt, until_dt=until_dt, pj_filter=filter2) == daily_report_str_pj111
-    filter3 = 'xxxx'
-    assert exp.export_daily_report(from_dt=from_dt, until_dt=until_dt, pj_filter=filter3) == '{}\n'
+    filter1 = ".*"
+    assert (
+        exp.export_daily_report(from_dt=from_dt, until_dt=until_dt, pj_filter=filter1)
+        == daily_report_str
+    )
+    filter2 = "pj1+"
+    assert (
+        exp.export_daily_report(from_dt=from_dt, until_dt=until_dt, pj_filter=filter2)
+        == daily_report_str_pj111
+    )
+    filter3 = "xxxx"
+    assert (
+        exp.export_daily_report(from_dt=from_dt, until_dt=until_dt, pj_filter=filter3)
+        == "{}\n"
+    )
 
 
 # raw structure from todoist api
 activity_logs_valid_data = {
-    'count': 3,
-    'events': [
+    "count": 3,
+    "events": [
         {
-            'event_date': '2020-12-27T02:30:40Z',
-            'event_type': 'completed',
-            'extra_data': {
-                'client': 'Mozilla/5.0; Todoist/1063',
-                'content': 'test1'
-            },
-            'id': 10600000001,
-            'initiator_id': None,
-            'object_id': 4000000001,
-            'object_type': 'item',
-            'parent_item_id': None,
-            'parent_project_id': 2000000001
+            "event_date": "2020-12-27T02:30:40Z",
+            "event_type": "completed",
+            "extra_data": {"client": "Mozilla/5.0; Todoist/1063", "content": "test1"},
+            "id": 10600000001,
+            "initiator_id": None,
+            "object_id": 4000000001,
+            "object_type": "item",
+            "parent_item_id": None,
+            "parent_project_id": 2000000001,
         },
         {
-            'event_date': '2020-12-27T01:30:40Z',
-            'event_type': 'completed',
-            'extra_data': {
-                'client': 'Mozilla/5.0; Todoist/1063',
-                'content': 'test2'
-            },
-            'id': 10600000002,
-            'initiator_id': None,
-            'object_id': 4000000002,
-            'object_type': 'item',
-            'parent_item_id': None,
-            'parent_project_id': 2000000001
+            "event_date": "2020-12-27T01:30:40Z",
+            "event_type": "completed",
+            "extra_data": {"client": "Mozilla/5.0; Todoist/1063", "content": "test2"},
+            "id": 10600000002,
+            "initiator_id": None,
+            "object_id": 4000000002,
+            "object_type": "item",
+            "parent_item_id": None,
+            "parent_project_id": 2000000001,
         },
         {
-            'event_date': '2020-12-25T01:30:40Z',
-            'event_type': 'completed',
-            'extra_data': {
-                'client': 'Mozilla/5.0; Todoist/1063',
-                'content': 'test3'
-            },
-            'id': 10600000003,
-            'initiator_id': None,
-            'object_id': 4000000003,
-            'object_type': 'item',
-            'parent_item_id': None,
-            'parent_project_id': 3000000001
-        }
-    ]
+            "event_date": "2020-12-25T01:30:40Z",
+            "event_type": "completed",
+            "extra_data": {"client": "Mozilla/5.0; Todoist/1063", "content": "test3"},
+            "id": 10600000003,
+            "initiator_id": None,
+            "object_id": 4000000003,
+            "object_type": "item",
+            "parent_item_id": None,
+            "parent_project_id": 3000000001,
+        },
+    ],
 }
 
 # activity event data
 activity_valid_data = [
     {
-        'event_date': '2020-12-27T02:30:40Z',
-        'event_type': 'completed',
-        'extra_data': {
-            'client': 'Mozilla/5.0; Todoist/1063',
-            'content': 'test1'
-        },
-        'id': 10600000001,
-        'initiator_id': None,
-        'object_id': 4000000001,
-        'object_type': 'item',
-        'parent_item_id': None,
-        'parent_project_id': 2000000001
+        "event_date": "2020-12-27T02:30:40Z",
+        "event_type": "completed",
+        "extra_data": {"client": "Mozilla/5.0; Todoist/1063", "content": "test1"},
+        "id": 10600000001,
+        "initiator_id": None,
+        "object_id": 4000000001,
+        "object_type": "item",
+        "parent_item_id": None,
+        "parent_project_id": 2000000001,
     },
     {
-        'event_date': '2020-12-27T01:30:40Z',
-        'event_type': 'completed',
-        'extra_data': {
-            'client': 'Mozilla/5.0; Todoist/1063',
-            'content': 'test2'
-        },
-        'id': 10600000002,
-        'initiator_id': None,
-        'object_id': 4000000002,
-        'object_type': 'item',
-        'parent_item_id': None,
-        'parent_project_id': 2000000001
+        "event_date": "2020-12-27T01:30:40Z",
+        "event_type": "completed",
+        "extra_data": {"client": "Mozilla/5.0; Todoist/1063", "content": "test2"},
+        "id": 10600000002,
+        "initiator_id": None,
+        "object_id": 4000000002,
+        "object_type": "item",
+        "parent_item_id": None,
+        "parent_project_id": 2000000001,
     },
     {
-        'event_date': '2020-12-25T01:30:40Z',
-        'event_type': 'completed',
-        'extra_data': {
-            'client': 'Mozilla/5.0; Todoist/1063',
-            'content': 'test3'
-        },
-        'id': 10600000003,
-        'initiator_id': None,
-        'object_id': 4000000003,
-        'object_type': 'item',
-        'parent_item_id': None,
-        'parent_project_id': 3000000001
-    }
+        "event_date": "2020-12-25T01:30:40Z",
+        "event_type": "completed",
+        "extra_data": {"client": "Mozilla/5.0; Todoist/1063", "content": "test3"},
+        "id": 10600000003,
+        "initiator_id": None,
+        "object_id": 4000000003,
+        "object_type": "item",
+        "parent_item_id": None,
+        "parent_project_id": 3000000001,
+    },
 ]
 
 activity_valid_data_str = """- event_date: '2020-12-27T02:30:40Z'
@@ -198,29 +191,30 @@ daily_report_str_pj111 = """'2020-12-27':
     name: test2
 """
 
+
 def get_project_side_effect(project_id):
     d = {
-        '2000000001': {
-            'child_order': 0,
-            'collapsed': 0,
-            'color': 1,
-            'id': 2000000001,
-            'is_archived': 0,
-            'is_deleted': 0,
-            'is_favorite': 0,
-            'name': 'pj111',
-            'parent_id': None
+        "2000000001": {
+            "child_order": 0,
+            "collapsed": 0,
+            "color": 1,
+            "id": 2000000001,
+            "is_archived": 0,
+            "is_deleted": 0,
+            "is_favorite": 0,
+            "name": "pj111",
+            "parent_id": None,
         },
-        '3000000001': {
-            'child_order': 0,
-            'collapsed': 0,
-            'color': 1,
-            'id': 3000000001,
-            'is_archived': 0,
-            'is_deleted': 0,
-            'is_favorite': 0,
-            'name': 'pj222',
-            'parent_id': None
-        }
+        "3000000001": {
+            "child_order": 0,
+            "collapsed": 0,
+            "color": 1,
+            "id": 3000000001,
+            "is_archived": 0,
+            "is_deleted": 0,
+            "is_favorite": 0,
+            "name": "pj222",
+            "parent_id": None,
+        },
     }
     return d[str(project_id)]
